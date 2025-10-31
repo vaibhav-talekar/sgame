1529a62d370182a0368de22a44e2160562d5cf1d
Vaibhav Talekar <mca2024.vaibhav@unigoa.ac.in>
2025-10-31 19:03:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`✅ Game server running on port ${port}`);
-});
+app.listen(port, () => console.log(`✅ Game running on port ${port}`));
